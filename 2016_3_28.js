6ee61daa4f9ef888ec93c5201c08432398d65a9e
FunctionRun <545418969@qq.com>
2016-03-29 13:47:07 +0800

    init

---
+++ 2016_3_28.js
@@ -0,0 +1,37 @@
+/*
+* @Author: zhangyujie
+* @Date:   2016-03-28 13:15:41
+* @Last Modified by:   FunctionRun
+* @Last Modified time: 2016-03-28 16:40:13
+* @Email: zhangyujie3344521@163.com
+* @File Path: /Users/zhangyujie/node/www/ycharts/2016_3_28.js
+* @File Name: 2016_3_28.js
+*/
+
+'use strict';
+
+//components
+const option = {
+	title: {},
+	subtitle: {},
+	tooltip: {},
+	legend: {},
+	xAxis: {},
+	yAxis: {},
+	grid: {},
+	label: {},
+	series: []
+}
+
+render() {
+
+	<Title />
+	<Subtitle />
+	<Tooltip />
+	<Legend />
+	<xAxis />
+	<yAxis />
+	<Label />
+	<Grid />
+	<Chart />
+}
